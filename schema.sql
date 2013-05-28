3fe0e3e302ec15b9dce093daef0ea64d9c5c2b4c
Ryan Galloway <ryan@rsgalloway.com>
2013-05-27 17:44:15 -0700

    tweaks to schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
-drop table if exists entries;
-create table entries (
-  id integer primary key autoincrement,
-  title string not null,
-  text string not null
+DROP TABLE IF EXISTS `entries`;
+CREATE TABLE `entries` (
+  `id` int(25) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `text` text NOT NULL,
+  PRIMARY KEY `id` (`id`)
 );
-
